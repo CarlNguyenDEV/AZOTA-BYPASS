9940163e41ea44a687d0cebfac8a930c3a9f6490
Carl Nguyen <162815397+DangNhutNguyen@users.noreply.github.com>
2024-07-12 23:26:19 +0700

    Add files via upload

---
+++ Code/Code/main.py
@@ -0,0 +1,209 @@
+class colors:
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    BLUE = '\033[34m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
+import pyautogui as pag
+import time
+import pyfiglet
+def main():
+    # Open Termial
+    time.sleep(time1)
+    pag.hotkey('ctrl', 'shift', 'i')
+    time.sleep(1)
+    # Open Element
+    pag.locateOnScreen("Element0.png", confidence=0.7)
+    time.sleep(0.5)
+    pag.click("Element0.png")
+    time.sleep(0.5)
+    # Open Events Listener
+    pag.locateOnScreen("EL0.png", confidence=0.7)
+    time.sleep(1)
+    pag.click("EL0.png")
+    time.sleep(0.2)
+    #2P
+    # pag.locateOnScreen("1.png", confidence=0.7)
+    # pointclick1 = (pag.locateOnScreen("Remove_2.png", confidence=0.7).left + 88, pag.locateOnScreen("Remove_2.png", confidence=0.7).top + 10)
+    # time.sleep(0.1)
+    # pag.click(pointclick1)
+    # time.sleep(0.1)
+    #DOM
+    pag.locateOnScreen("2.png", confidence=0.5)
+    time.sleep(0.1)
+    pag.click("2.png")
+    pointclick2 = (pag.locateOnScreen("Remove_2.png", confidence=0.7).left + 88, pag.locateOnScreen("Remove_2.png", confidence=0.7).top + 10)
+    time.sleep(0.1)
+    pag.click(pointclick2)
+    time.sleep(0.1)
+    pag.click(pointclick2)
+    time.sleep(0.1)
+    #Before
+    pag.locateOnScreen("3.png", confidence=0.5)
+    time.sleep(0.1)
+    pag.click("3.png")
+    pointclick3 = (pag.locateOnScreen("Remove_2.png", confidence=0.7).left + 88, pag.locateOnScreen("Remove_2.png", confidence=0.7).top + 10)
+    time.sleep(0.1)
+    pag.click(pointclick3)
+    time.sleep(0.1)
+    #Click
+    pag.locateOnScreen("4.png", confidence=0.5)
+    time.sleep(0.1)
+    pag.click("4.png")
+    pointclick4 = (pag.locateOnScreen("Remove_2.png", confidence=0.7).left + 88, pag.locateOnScreen("Remove_2.png", confidence=0.7).top + 10)
+    time.sleep(0.1)
+    pag.click(pointclick4)
+    time.sleep(0.1)
+    #Hashchange
+    pag.locateOnScreen("5.png", confidence=0.3)
+    time.sleep(0.1)
+    pag.click("5.png")
+    pointclick5 = (pag.locateOnScreen("Remove_2.png", confidence=0.7).left + 88, pag.locateOnScreen("Remove_2.png", confidence=0.7).top + 10)
+    time.sleep(0.1)
+    pag.click(pointclick5)
+    time.sleep(0.1)
+    #Load
+    pag.locateOnScreen("6.png", confidence=0.5)
+    time.sleep(0.1)
+    pag.click("6.png")
+    pointclick6 = (pag.locateOnScreen("Remove_2.png", confidence=0.7).left + 88, pag.locateOnScreen("Remove_2.png", confidence=0.7).top + 10)
+    time.sleep(0.1)
+    pag.click(pointclick6)
+    time.sleep(0.1)
+    pag.click(pointclick6)
+    time.sleep(0.1)
+    #Locatech
+    pag.locateOnScreen("7.png", confidence=0.5)
+    time.sleep(0.1)
+    pag.click("7.png")
+    pointclick7 = (pag.locateOnScreen("Remove_2.png", confidence=0.7).left + 88, pag.locateOnScreen("Remove_2.png", confidence=0.7).top + 10)
+    time.sleep(0.1)
+    pag.click(pointclick7)
+    time.sleep(0.1)
+    #Orient
+    pag.locateOnScreen("9.png", confidence=0.5)
+    time.sleep(0.1)
+    pag.click("9.png")
+    pointclick9 = (pag.locateOnScreen("Remove_2.png", confidence=0.7).left + 88, pag.locateOnScreen("Remove_2.png", confidence=0.7).top + 10)
+    time.sleep(0.1)
+    pag.click(pointclick9)
+    time.sleep(0.3)
+    #Popstate
+    pag.locateOnScreen("10.png", confidence=0.6)
+    time.sleep(0.3)
+    pag.click("10.png")
+    pointclick10 = (pag.locateOnScreen("Remove_2.png", confidence=0.7).left + 88, pag.locateOnScreen("Remove_2.png", confidence=0.7).top + 10)
+    time.sleep(0.1)
+    pag.click(pointclick10)
+    time.sleep(0.1)
+    #Message
+    pag.locateOnScreen("8.png", confidence=0.6)
+    time.sleep(0.3)
+    pag.click("8.png")
+    time.sleep(0.1)
+    pointclick8 = (pag.locateOnScreen("Remove_2.png", confidence=0.7).left + 88, pag.locateOnScreen("Remove_2.png", confidence=0.7).top + 10)
+    time.sleep(0.1)
+    pag.click(pointclick8)
+    time.sleep(0.1)
+    #Resize
+    pag.locateOnScreen("11.png", confidence=0.6)
+    time.sleep(0.3)
+    pag.click("11.png")
+    pointclick11 = (pag.locateOnScreen("Remove_2.png", confidence=0.7).left + 88, pag.locateOnScreen("Remove_2.png", confidence=0.7).top + 10)
+    time.sleep(0.1)
+    pag.click(pointclick11)
+    time.sleep(0.1)
+    pag.click(pointclick11)
+    time.sleep(0.1)
+    #Scroll
+    pag.locateOnScreen("12.png", confidence=0.5)
+    time.sleep(0.3)
+    pag.click("12.png")
+    pointclick12 = (pag.locateOnScreen("Remove_2.png", confidence=0.7).left + 88, pag.locateOnScreen("Remove_2.png", confidence=0.7).top + 10)
+    time.sleep(0.1)
+    pag.click(pointclick12)
+    time.sleep(0.1)
+
+    pag.locateOnScreen("X.png", confidence=0.7)
+    time.sleep(0.1)
+    pag.click("X.png")
+    time.sleep(0.2)
+    pag.hotkey('esc')
+# Title
+text = "Azota Bypassv1.2"
+ascii_art = pyfiglet.figlet_format(text)
+print(colors.BLUE + ascii_art + colors.END)
+print(colors.RED + "Language:" + colors.END, end=' ')
+# Language
+lang = int(input(" 1-Vietnamese ; 2-English: "))
+# Infomation1
+if lang == 1:
+    print(colors.RED + "Quản lý: " + colors.END, end=' ')
+    print("Nguyễn Huỳnh Đăng Nhựt")
+    print(colors.RED + "Liên hệ: " + colors.END, end=' ')
+    print("penciltalk0910@gmail.com")
+    password1 = int(input(colors.RED + "Vui lòng nhập mật khẩu: " + colors.END))
+# Infomation 2
+    while True:
+        if password1 == 122333445:
+            print("")
+            print(colors.BLUE + "Chúc bạn sử dụng phần mềm vui vẻ" + colors.END)
+            print(colors.RED + 'Lưu ý: Phần mềm chỉ sử dụng dưới mục đích học tập và tìm hiểu, không dùng phần mềm dưới mục đính thực hiện trái phép các hoạt động trong kì thi' + colors.END)
+            print(colors.RED + '"Bạn đồng ý chứ:' + colors.END)
+            Ok1 = int(input( "1-Đồng ý ; 2-Không đồng ý: "))
+            while True:
+                if Ok1 == 1:
+                    print(colors.BLUE + "Tôi ghi nhận ý kiến này từ bạn" + colors.END)
+                    time1 = float(input(colors.YELLOW + "Nhập thời gian chờ để (Kể từ thời gian bạn chạy chương trình cho đến khi dùng AZOTA (Khuyến khích: 5): " + colors.END))
+                    print("Đã ghi nhận thời gian chờ của bạn là: ", time1)
+                    time.sleep(1)
+                    while True:
+                        print(colors.YELLOW + 'Sau khi bắt đầu, bạn bật AZOTA và đợi khoảng chờ hệ thống sẽ chạy sau khoảng thời gian bạn đã định sẵn' + colors.END)
+                        do = int(input("Nhấn 1 để bắt đầu, 2 để quay lại thao tác: "))
+                        if do == 1:
+                            main()
+                            print(colors.BLUE + 'Thực hiện thành công' + colors.END)
+                        else:
+                            break
+                else:
+                    print("")
+                    print(colors.RED + "Nếu bạn không đồng ý thì không nên sử dụng phần mềm trái phép này" + colors.END)
+                    break
+        else:
+            print(colors.RED + "Mật khẩu sai, vui lòng liên hệ Admin để nhận đúng" + colors.END)
+            break
+else:
+        print(colors.RED + "Adminator: " + colors.END, end=' ')
+        print("Nguyễn Huỳnh Đăng Nhựt")
+        print(colors.RED + "Contact: " + colors.END, end=' ')
+        print("penciltalk0910@gmail.com")
+        password1 = int(input(colors.RED + "Please enter password: " + colors.END))
+        while True:
+            if password1 == 122333445:
+                print("")
+                print(colors.BLUE + "Good luck!" + colors.END)
+                print(colors.RED + "This tool only use for education. Please don't use this tool for your tests and crack some thing special" + colors.END)
+                print(colors.RED + 'Agree? ' + colors.END, end=' ')
+                Ok1 = int(input("1 - Agree; 2 - Disagree: "))
+                while True:
+                    if Ok1 == 1:
+                        print(colors.BLUE + "I recorded that" + colors.END)
+                        time1 = float(input(colors.YELLOW + "Enter time for waiting you open the AZOTA Website (Recommend: 5) " + colors.END))
+                        print("Your time is: ", time1)
+                        time.sleep(1)
+                        while True:
+                            print(colors.YELLOW + 'After you run this script, open the AZOTA tab before this script working' + colors.END)
+                            do = int(input("Press 1 to start, 2 to stop "))
+                            if do == 1:
+                                main()
+                                print(colors.BLUE + 'AZOTA Bypass Successfully work' + colors.END)
+                            else:
+                                break
+                    else:
+                        print("")
+                        print(colors.RED + "If you don't agree to this license, please don't work on it" + colors.END)
+                        break
+            else:
+                print(colors.RED + "Wrong password, Contact to admin for more" + colors.END)
+                break
